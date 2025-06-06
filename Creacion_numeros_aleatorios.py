e5fd92189d75d4b95fcd8bd11c4ff3ce89ee6ac6
KevinHurtadoDUOC <ke.hurtado@duocuc.cl>
2025-06-06 11:04:17 -0400

    version3
    Se agrega contador de numeros pares

--- Creacion_numeros_aleatorios.py
+++ Creacion_numeros_aleatorios.py
@@ -4,7 +4,13 @@ numero = random.randint(1,2) #margenes para el random.
 numeros = []
 
 for i in range(100):      #guardamos 100 nros randoms
-    numeros.append(random.randint)
-
+    numeros.append(random.randint(1,100))
 
 print(numeros)  #imprimimos la lista con los 100 nros generados
+
+cont = 0
+for i in range(len(numeros)):
+    if numeros[i]%2==0:
+        cont+=1
+
+print(f"Cantidad de pares en la lista : {cont}")
